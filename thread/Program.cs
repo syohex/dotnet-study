8a798e64f93efdfe273d6acd897b5f7b1ca026af
Shohei YOSHIDA <shohei.yoshida@dena.com>
2020-09-18 17:44:36 +0900

    Add thread example

---
+++ thread/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+
+namespace Sample
+{
+    class MyThread
+    {
+        public static void Do(object n)
+        {
+            var k = (int)n;
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"n + 1 = {k + i}");
+                Thread.Sleep(500);
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ParameterizedThreadStart p = MyThread.Do;
+            Thread t = new Thread(p);
+            t.Start(10);
+        }
+    }
+}
