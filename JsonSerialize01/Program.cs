3dda7899e8aee9b9703ded49235a11ad30e0fb9e
Shohei YOSHIDA <syohex@gmail.com>
2021-10-13 17:36:38 +0900

    Add Dictionary to JSON example

---
+++ JsonSerialize01/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+
+namespace JsonSerialize01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dict = new Dictionary<string, object> {
+                {"foo", "bar"},
+                {"baz", 20},
+                {"age", true},
+                {"hoge", null},
+            };
+
+            var json = JsonSerializer.Serialize(dict);
+            Console.WriteLine(json);
+        }
+    }
+}
