a96630b50e85d66bb05d9847d9e802bbbf116f63
Shohei YOSHIDA <syohex@gmail.com>
2021-10-15 16:09:52 +0900

    Add simple string trim test

---
+++ StringTrim01/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StringTrim01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "https://syohex.org/";
+            string stripped = text.Replace("https://", "").Trim('/');
+            Console.WriteLine(stripped);
+
+            text = "???foo***";
+            Console.WriteLine(text.TrimStart('?').TrimEnd('*'));
+        }
+    }
+}
