b07573072863b85681d5f65a066d12adb5672810
Shohei YOSHIDA <syohex@gmail.com>
2021-10-19 19:41:46 +0900

    Add string instance copy sample

---
+++ StringCopy/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StringCopy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "hello world";
+            string b = a;
+
+            Console.WriteLine($"a={a}({a.GetHashCode()}), b={b}({b.GetHashCode()})");
+            a += "k";
+            Console.WriteLine($"a={a}({a.GetHashCode()}), b={b}({b.GetHashCode()})");
+        }
+    }
+}
