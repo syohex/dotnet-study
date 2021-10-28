a7b4098a4ddd14c0a0898e093305c85f5392783e
Shohei YOSHIDA <syohex@gmail.com>
2021-10-28 15:05:05 +0900

    Add empty list join example

---
+++ EmptyListJoin/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EmptyListJoin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<String> { "a", "b", "c" };
+            var joint = string.Join(",", list);
+            Console.WriteLine($"joint = {joint}");
+
+            var empty_list = new List<String> { };
+            var empty_joint = string.Join(",", empty_list);
+            Console.WriteLine($"empty_joint = '{empty_joint}'");
+        }
+    }
+}
