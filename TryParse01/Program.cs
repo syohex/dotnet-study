968ab488fba5f86ca3a3efbf4f091e82702b0a5c
Shohei YOSHIDA <syohex@gmail.com>
2021-11-11 00:44:14 +0900

    Add TryParse sample

---
+++ TryParse01/Program.cs
@@ -0,0 +1,20 @@
+﻿var input = new List<string>
+{
+    "12345",
+    "1e40",
+    "1E40",
+    "1E307",
+    "3.141592653",
+};
+
+foreach (var s in input)
+{
+    if (Double.TryParse(s, out double value))
+    {
+        Console.WriteLine($"TryParse({s}) ok: {value}");
+    }
+    else
+    {
+        Console.WriteLine($"TryParse({s}) failed: {value}");
+    }
+}
