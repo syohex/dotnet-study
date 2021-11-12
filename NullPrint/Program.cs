634c323ad857394e408c401ca0b8ddda05f52fe3
Shohei YOSHIDA <syohex@gmail.com>
2021-11-12 21:56:55 +0900

    Add null print sample

---
+++ NullPrint/Program.cs
@@ -0,0 +1,14 @@
+﻿int? a = null;
+string? b = null;
+
+// If value is null, nothing display
+Console.WriteLine($"a='{a}'");
+Console.WriteLine($"b='{b}'");
+
+// condition expression cannot be used in string interpolation
+if (b == null) {
+    Console.WriteLine("b = null");
+} else {
+    Console.WriteLine($"b = {b}");
+}
+
