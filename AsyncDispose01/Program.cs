05ece2d9041c0f95938835465e45a72051190466
Shohei YOSHIDA <syohex@gmail.com>
2021-11-26 18:34:45 +0900

    Add Dispose + async/await sample

---
+++ AsyncDispose01/Program.cs
@@ -0,0 +1,25 @@
+﻿await TestFunc();
+
+static async Task TestFunc()
+{
+    using (var dispose = new TestAsync())
+    {
+        Console.WriteLine("Before RunAsync");
+        await dispose.RunAsync();
+        Console.WriteLine("After RunAsync");
+    }
+}
+
+public class TestAsync : IDisposable
+{
+    public void Dispose()
+    {
+        Console.WriteLine("Dispose");
+    }
+
+    public async Task RunAsync()
+    {
+        Console.WriteLine("RunAsync");
+        await Task.Delay(2000);
+    }
+}
