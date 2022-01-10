79256c791c797bac70541d2095365ec7fb26a7af
Shohei YOSHIDA <syohex@gmail.com>
2022-01-10 22:03:46 +0900

    Add section 7

--- fsharp/programming_haskell/section07/main.fsx
+++ fsharp/programming_haskell/section07/main.fsx
@@ -57,3 +57,54 @@ let rec dropWhile f xs =
     | _ :: t -> dropWhile f t
 
 dropWhile (fun n -> n % 2 = 1) [ 1; 3; 5; 6; 7 ]
+
+// 7.3
+let rec foldr f v xs =
+    match xs with
+    | [] -> v
+    | h :: t -> f h (foldr f v t)
+
+let sum xs = foldr (+) 0 xs
+
+sum [ 1 .. 100 ]
+
+// 7.4
+let rec foldl f v xs =
+    match xs with
+    | [] -> v
+    | h :: t -> foldl f (f v h) t
+
+let length xs = foldl (fun acc _ -> 1 + acc) 0 xs
+
+length [ 1 .. 100 ]
+
+// 7.6
+let bin2int xs =
+    xs
+    |> List.rev
+    |> List.fold (fun acc n -> (2 * acc) + n) 0
+
+bin2int [ 1; 0; 1; 1 ]
+
+let rec int2bin n =
+    if n = 0 then
+        []
+    else
+        (n % 2) :: int2bin (n / 2)
+
+int2bin 13
+
+// 7.9
+let myMap2 f xs = foldr (fun x acc -> (f x) :: acc) [] xs
+
+myMap2 ((+) 1) [ 1 .. 10 ]
+
+let myFilter2 f xs =
+    foldr (fun x acc -> if f x then x :: acc else acc) [] xs
+
+myFilter2 (fun n -> n % 2 = 0) [ 1 .. 10 ]
+
+let dec2int ns =
+    ns |> List.fold (fun acc n -> 10 * acc + n) 0
+
+dec2int [ 2; 3; 4; 5 ]
