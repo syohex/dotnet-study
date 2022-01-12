6a943d47a602d6c2c3138a72107c4a6a282be258
Shohei YOSHIDA <syohex@gmail.com>
2022-01-12 14:41:27 +0900

    Add very simple file read program like cat

---
+++ fsharp/file01/Program.fs
@@ -0,0 +1,13 @@
+﻿open System.IO
+// For more information see https://aka.ms/fsharp-console-apps
+
+[<EntryPoint>]
+let main args =
+    let lines = args |> Array.map File.ReadAllLines
+
+    let contents =
+        lines
+        |> Array.map (fun ls -> System.String.Join("\n", ls))
+
+    contents |> Array.iter (fun a -> printfn "%s\n" a)
+    0
