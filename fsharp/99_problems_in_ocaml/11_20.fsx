d240d9f4bef8634cb0ed5be4e992968092e0b2ad
Shohei YOSHIDA <syohex@gmail.com>
2022-02-15 08:45:30 +0900

    Add problem 11

---
+++ fsharp/99_problems_in_ocaml/11_20.fsx
@@ -0,0 +1,41 @@
+// problem 11
+type Rle<'a> =
+    | One of 'a
+    | Many of int * 'a
+
+let encode2 xs =
+    let rec encode2' xs prev count acc =
+        match xs with
+        | [] ->
+            if count = 1 then
+                ((One prev) :: acc) |> List.rev
+            else
+                ((Many(count, prev)) :: acc) |> List.rev
+        | y :: ys ->
+            if prev = y then
+                encode2' ys prev (count + 1) acc
+            else if count = 1 then
+                encode2' ys y 1 ((One prev) :: acc)
+            else
+                encode2' ys y 1 ((Many(count, prev)) :: acc)
+
+    match xs with
+    | [] -> []
+    | y :: ys -> encode2' ys y 1 []
+
+//[Many (4, "a"); One "b"; Many (2, "c"); Many (2, "a"); One "d";
+// Many (4, "e")]
+encode2 [ "a"
+          "a"
+          "a"
+          "a"
+          "b"
+          "c"
+          "c"
+          "a"
+          "a"
+          "d"
+          "e"
+          "e"
+          "e"
+          "e" ]
