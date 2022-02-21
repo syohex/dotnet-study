5b6f262e275e9ebe24a41ad7d45c13549f8db5c1
Shohei YOSHIDA <syohex@gmail.com>
2022-02-21 11:34:16 +0900

    Add new answer of problem 169

--- fsharp/leetcode/problems/0169/main.fsx
+++ fsharp/leetcode/problems/0169/main.fsx
@@ -1,5 +1,23 @@
-let majorityElement (nums: int list) : int =
-    nums |> List.countBy id |> List.sortBy (fun (_, count) -> count) |> List.rev |> List.head |> fst
+let majorityElement1 (nums: int list) : int =
+    nums
+    |> List.countBy id
+    |> List.sortBy (fun (_, count) -> count)
+    |> List.rev
+    |> List.head
+    |> fst
 
-majorityElement [3;2;3]
-majorityElement [2;2;1;1;1;2;2]
+let majorityElement (nums: int list) : int =
+    let arr = nums |> List.sort |> List.toArray
+    arr.[(Array.length arr) / 2]
+
+// 3
+majorityElement1 [ 3; 2; 3 ]
+majorityElement [ 3; 2; 3 ]
+
+// 2
+majorityElement1 [ 2; 2; 1; 1; 1; 2; 2 ]
+majorityElement [ 2; 2; 1; 1; 1; 2; 2 ]
+
+// 1
+majorityElement1 [ 1 ]
+majorityElement [ 1 ]
