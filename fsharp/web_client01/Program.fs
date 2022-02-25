08da50e06436b2006b21fd441aa99164bbe83bcf
Shohei YOSHIDA <syohex@gmail.com>
2022-02-25 15:56:24 +0900

    Add web client example

---
+++ fsharp/web_client01/Program.fs
@@ -0,0 +1,24 @@
+﻿open System
+open System.Net.Http
+
+let downloadURL (url: Uri) =
+    async {
+        let client = new HttpClient()
+        let! content = client.GetStringAsync(url) |> Async.AwaitTask
+        return content
+    }
+
+[<EntryPoint>]
+let main args =
+    let url =
+        if args.Length >= 1 then
+            args.[0]
+        else
+            Console.ReadLine()
+
+    let content =
+        downloadURL (new Uri(url))
+        |> Async.RunSynchronously
+
+    printfn $"{content}"
+    0
