211401e809c556f2549f5f3ec8f4cdc8649e5156
Shohei YOSHIDA <syohex@gmail.com>
2022-02-27 23:24:26 +0900

    Add scraping sample

---
+++ fsharp/scraping02/Program.fs
@@ -0,0 +1,12 @@
+﻿open FSharp.Data
+
+let getTitle (url: string) : string =
+    let doc = HtmlDocument.Load(url)
+    let title = doc.CssSelect("head > title")
+    title.[0].InnerText()
+
+[<EntryPoint>]
+let main args =
+    let title = getTitle args.[0]
+    printfn $"title: {title}"
+    0
