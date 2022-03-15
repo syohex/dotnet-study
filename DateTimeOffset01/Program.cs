a33fde8428ea9aae30a44a8ac6978a981bc19c16
Shohei YOSHIDA <syohex@gmail.com>
2022-03-15 20:03:24 +0900

    Add DateTimeOffset sample

---
+++ DateTimeOffset01/Program.cs
@@ -0,0 +1,8 @@
+﻿var now = DateTimeOffset.Now;
+var from = DateTimeOffset.FromUnixTimeSeconds(1647253699);
+
+if (from < now) {
+    Console.WriteLine("It is not expired");
+} else {
+    Console.WriteLine("expired");
+}
