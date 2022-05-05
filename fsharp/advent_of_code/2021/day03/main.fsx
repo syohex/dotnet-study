1e6bde6a9825060c9eaafc71c67d2b8a0e8c140d
Shohei YOSHIDA <syohex@gmail.com>
2022-05-05 22:55:49 +0900

    Add day3 part2

--- fsharp/advent_of_code/2021/day03/main.fsx
+++ fsharp/advent_of_code/2021/day03/main.fsx
@@ -24,18 +24,68 @@ let problem1 (reports: string list) : int =
     let len = reports.Head.Length
     problem1' 0 len reports 0 0
 
+let rec problem2' (n: int) (isOxygen: bool) (reports: string list) : int =
+    if (reports |> List.tail |> List.isEmpty) then
+        reports
+        |> List.head
+        |> Seq.fold (fun acc c -> acc * 2 + (if c = '1' then 1 else 0)) 0
+    else
+        let zeros, ones =
+            reports
+            |> List.fold
+                (fun (zeros, ones) report ->
+                    if report.[n] = '0' then
+                        zeros + 1, ones
+                    else
+                        zeros, ones + 1)
+                (0, 0)
+
+        if isOxygen then
+            if zeros > ones then
+                let reports' =
+                    reports |> List.filter (fun s -> s.[n] = '0')
+
+                problem2' (n + 1) isOxygen reports'
+            else
+                let reports' =
+                    reports |> List.filter (fun s -> s.[n] = '1')
+
+                problem2' (n + 1) isOxygen reports'
+        else if zeros <= ones then
+            let reports' =
+                reports |> List.filter (fun s -> s.[n] = '0')
+
+            problem2' (n + 1) isOxygen reports'
+        else
+            let reports' =
+                reports |> List.filter (fun s -> s.[n] = '1')
+
+            problem2' (n + 1) isOxygen reports'
+
+let problem2 (reports: string list) : int =
+    let oxygen = problem2' 0 true reports
+    let co2 = problem2' 0 false reports
+    oxygen * co2
+
 let test () =
     let lines =
         File.ReadLines("test1.txt")
         |> Seq.filter (fun line -> String.IsNullOrWhiteSpace line |> not)
         |> Seq.toList
 
-    let ret = problem1 lines
+    let ret1 = problem1 lines
 
-    if ret <> 198 then
-        failwith $"answer is {ret}: expected: 198"
+    if ret1 <> 198 then
+        failwith $"answer1 is {ret1}: expected: 198"
     else
-        printf "OK\n"
+        printf "OK1\n"
+
+    let ret2 = problem2 lines
+
+    if ret2 <> 230 then
+        failwith $"answer2 is {ret2}: expected: 230"
+    else
+        printf "OK2\n"
 
 test ()
 
@@ -45,8 +95,13 @@ let inputs =
     |> Seq.toList
 
 let ret1 = problem1 inputs
+let ret2 = problem2 inputs
 
 if ret1 <> 3912944 then
     failwith $"problem1: {ret1}, expect: 3912944"
 
+if ret2 <> 4996233 then
+    failwith $"problem2: {ret2}, expect: 4996233"
+
 printfn $"problem1: {ret1}"
+printfn $"problem2: {ret2}"
