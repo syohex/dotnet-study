9f5942a31542f35a824d2d7d8975a37e17a67263
Shohei YOSHIDA <syohex@gmail.com>
2022-05-08 11:04:40 +0900

    Add problem 341

---
+++ fsharp/leetcode/problems/0341/main.fsx
@@ -0,0 +1,52 @@
+type NestedInteger =
+    | Int of int
+    | List of NestedInteger list
+
+type NestedIterator =
+    { mutable Nums: int list }
+
+    static member init(nestedList: NestedInteger list) : NestedIterator =
+        let rec flatten nestedList acc =
+            match nestedList with
+            | [] -> acc |> List.rev
+            | h :: t ->
+                match h with
+                | Int (n) -> flatten t (n :: acc)
+                | List (v) ->
+                    let lst = flatten v []
+                    let acc' = (lst |> List.rev) @ acc
+                    flatten t acc'
+
+        let nums = flatten nestedList []
+        { Nums = nums }
+
+    member this.next() : int =
+        let ret = List.head this.Nums
+        this.Nums <- List.tail this.Nums
+        ret
+
+    member this.hasNext() : bool = List.isEmpty this.Nums |> not
+
+let check (ni: NestedIterator) : int list =
+    seq {
+        while ni.hasNext () do
+            yield ni.next ()
+    }
+    |> Seq.toList
+
+let lst1 =
+    [ List([ Int(1); Int(1) ])
+      Int(2)
+      List([ Int(1); Int(1) ]) ]
+
+let ni = NestedIterator.init lst1
+// [1;1;2;1;1]
+check ni
+
+let lst2 =
+    [ Int(1)
+      List([ Int(4); List([ Int(6) ]) ]) ]
+
+let ni2 = NestedIterator.init lst2
+// [1;4;6]
+check ni2
