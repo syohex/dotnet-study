a50fb2cffff7a870b142cd25021555cc2bfcd515
Shohei YOSHIDA <syohex@gmail.com>
2022-05-23 16:04:28 +0900

    Add Assembly class sample

---
+++ Assembly01/Program.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+
+var file = args[0];
+Console.WriteLine($"file={file}");
+
+var ass = Assembly.LoadFrom(file);
+Console.WriteLine($"runtime version: {ass.ImageRuntimeVersion}");
