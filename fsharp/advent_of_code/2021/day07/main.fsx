38b7ea82f8812dcf45472759a5dc342dbbbf6f54
Shohei YOSHIDA <syohex@gmail.com>
2022-06-02 12:47:58 +0900

    Add Day07 part2

--- fsharp/advent_of_code/2021/day07/main.fsx
+++ fsharp/advent_of_code/2021/day07/main.fsx
@@ -28,10 +28,36 @@ let part1 (nums: int list) : int =
     let min, max = nums |> List.min, nums |> List.max
     part1' min max nums Int32.MaxValue
 
+let part2 (nums: int list) : int =
+    let rec part2' pos max (nums: int list) (fuels: int []) (ret: int) =
+        if pos > max then
+            ret
+        else
+            let ret' =
+                nums
+                |> List.fold (fun acc n -> acc + fuels.[Math.Abs(n - pos)]) 0
+
+            part2' (pos + 1) max nums fuels (Math.Min(ret, ret'))
+
+    let min, max = nums |> List.min, nums |> List.max
+
+    let fuels =
+        seq { 0 .. (max - min) }
+        |> Seq.map (fun n -> seq { 1 .. n } |> Seq.sum)
+        |> Seq.toArray
+
+    part2' min max nums fuels Int32.MaxValue
+
 let test = parseInput "test.txt"
 // 37
 part1 test
 
+// 168
+part2 test
+
 let input = parseInput "input.txt"
 // 359648
 part1 input
+
+// 100727924
+part2 input
