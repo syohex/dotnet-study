50d3498d6ca99656cc9e03731eb4fe08b97b431e
Shohei YOSHIDA <syohex@gmail.com>
2022-11-14 10:55:22 +0900

    Add problem 2469

---
+++ fsharp/leetcode/problems/2469-convert-the-temperature/main.fsx
@@ -0,0 +1,7 @@
+let convertTemperature (celsius: double) : double * double = celsius + 273.15, celsius * 1.8 + 32.0
+
+// (309.65, 97.7)
+convertTemperature 36.5
+
+// (395.26, 251.798)
+convertTemperature 122.11
