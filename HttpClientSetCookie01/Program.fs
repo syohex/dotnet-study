766942b4d01e6ae2cb95b3ce9cd50a2441f388b3
Shohei YOSHIDA <syohex@gmail.com>
2023-02-26 18:41:11 +0900

    Add set cookie example

---
+++ HttpClientSetCookie01/Program.fs
@@ -0,0 +1,31 @@
+﻿open System
+open System.Net.Http
+
+let getContent url =
+    async {
+        use client = new HttpClient()
+        use handler = new HttpClientHandler()
+
+        client.BaseAddress <- url
+        handler.UseCookies <- false
+
+        let message =
+            new HttpRequestMessage(HttpMethod.Get, "/")
+
+        message.Headers.Add("Cookie", "key=value")
+
+        let! response = client.SendAsync(message) |> Async.AwaitTask
+
+        return!
+            response.Content.ReadAsStringAsync()
+            |> Async.AwaitTask
+    }
+
+[<EntryPoint>]
+let main args : int =
+    let content =
+        getContent (new Uri("https://syohex.org"))
+        |> Async.RunSynchronously
+
+    printfn $"{content}"
+    0
