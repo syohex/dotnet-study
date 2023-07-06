7f1c6f9b777e34602eaa11b825bb06c9832e940a
Shohei YOSHIDA <syohex@gmail.com>
2023-07-06 12:49:07 +0900

    Add TaskCompletionSource example

---
+++ TaskCompletionSource01/Program.cs
@@ -0,0 +1,21 @@
+﻿class TaskCompletionSourceDemo
+{
+    static async Task Main()
+    {
+        TaskCompletionSource<int> tcs1 = new TaskCompletionSource<int>();
+        Task<int> t1 = tcs1.Task;
+
+        // Start a background task that will complete tcs1.Task
+        var _ = Task.Factory.StartNew(() =>
+        {
+            Console.WriteLine("## Before Sleep");
+            Thread.Sleep(1000);
+            Console.WriteLine("## After Sleep");
+            tcs1.SetResult(15);
+        });
+
+        Console.WriteLine("## AWAIT");
+        int result = await t1;
+        Console.WriteLine($"result={result}");
+    }
+}
