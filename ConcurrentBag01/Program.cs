834d2c20b5f7da303badb97a41c64a029fac9653
Shohei YOSHIDA <syohex@gmail.com>
2023-07-10 17:34:01 +0900

    Add ConcurrentBag sample

---
+++ ConcurrentBag01/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Concurrent;
+
+ConcurrentBag<int> a = new();
+a.Add(1);
+a.Add(2);
+a.Add(3);
+a.Add(4);
+a.Add(5);
+a.Add(6);
+
+while (a.TryTake(out var b)) {
+    Console.WriteLine($"Take value={b}");
+}
