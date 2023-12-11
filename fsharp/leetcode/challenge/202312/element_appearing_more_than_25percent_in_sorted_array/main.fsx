a02741c46fbb0a46e637b9ce0718946e5c5f86e5
Shohei YOSHIDA <syohex@gmail.com>
2023-12-11 09:47:59 +0900

    Add element appearing more than 25% in sorted array

---
+++ fsharp/leetcode/challenge/202312/element_appearing_more_than_25percent_in_sorted_array/main.fsx
@@ -0,0 +1,12 @@
+let findSpecialInteger (arr: int list) : int =
+    arr
+    |> List.windowed ((List.length arr / 4) + 1)
+    |> List.filter (fun a -> List.head a = List.last a)
+    |> List.head
+    |> List.head
+
+// 6
+findSpecialInteger [1;2;2;6;6;6;6;7;10]
+
+// 1
+findSpecialInteger [1;1]
