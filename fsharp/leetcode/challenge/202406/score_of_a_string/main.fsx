167469dc29b0476d5389f0ac4efae3f4d5124e5c
Shohei YOSHIDA <syohex@gmail.com>
2024-06-01 23:10:38 +0900

    Add score of a string

---
+++ fsharp/leetcode/challenge/202406/score_of_a_string/main.fsx
@@ -0,0 +1,17 @@
+let scoreOfString (s: string) : int =
+    let rec scoreOfString' cs prev acc =
+        match cs with
+        | [] -> acc
+        | h :: t -> scoreOfString' t h (acc + abs (h - prev))
+
+    let cs = s |> Seq.map int |> Seq.toList
+
+    match cs with
+    | [] -> failwith "never reach here"
+    | h :: t -> scoreOfString' t h 0
+
+// 13
+scoreOfString "hello"
+
+// 50
+scoreOfString "zaz"
