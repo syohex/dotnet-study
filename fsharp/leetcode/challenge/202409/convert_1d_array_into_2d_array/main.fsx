560bff6b75ac37b632de2a23db182ead73f26cb0
Shohei YOSHIDA <syohex@gmail.com>
2024-09-01 14:14:58 +0900

    Add convert 1D array into 2D array

---
+++ fsharp/leetcode/challenge/202409/convert_1d_array_into_2d_array/main.fsx
@@ -0,0 +1,21 @@
+let construct2DArray (original: int list) (m: int) (n: int) : int[,] =
+    let rec construct2DArray' original (acc: int[,]) =
+        match original with
+        | [] -> acc
+        | (i, h) :: t ->
+            acc.[i / n, i % n] <- h
+            construct2DArray' t acc
+
+    if List.length original <> m * n then
+        Array2D.zeroCreate 0 0
+    let acc = Array2D.zeroCreate m n
+    construct2DArray' (List.indexed original) acc
+
+// [[1,2],[3,4]]
+construct2DArray [1;2;3;4] 2 2
+
+// [[1,2,3]]
+construct2DArray [1;2;3] 1 3
+
+// [[]]
+construct2DArray [1;2] 1 1
