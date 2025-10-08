3af71c17b55a2f4ed808b4eb0aa42678f461ff2d
Shohei YOSHIDA <syohex@gmail.com>
2025-10-08 11:38:25 +0900

    Add successful pairs of spells and potions

---
+++ fsharp/leetcode/challenge/202510/successful_pairs_of_spells_and_potions/main.fsx
@@ -0,0 +1,25 @@
+let successfulPairs (spells: int list) (potions: int list) (success: int64) : int list =
+    let rec lowerBound left right spell (vals: int[]) =
+        if left >= right then
+            left
+        else
+            let mid = left + (right - left) / 2
+
+            if int64 (spell * vals.[mid]) < success then
+                lowerBound (mid + 1) right spell vals
+            else
+                lowerBound left mid spell vals
+
+    let potions = potions |> List.sort |> List.toArray
+    let len = potions.Length
+
+    spells
+    |> List.map (fun spell ->
+        let pos = lowerBound 0 len spell potions
+        len - pos)
+
+// [4,0,3]
+successfulPairs [ 5; 1; 3 ] [ 1; 2; 3; 4; 5 ] 7L
+
+// [2,0,2]
+successfulPairs [ 3; 1; 2 ] [ 8; 5; 8 ] 16L
